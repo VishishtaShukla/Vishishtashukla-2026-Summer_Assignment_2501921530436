885cc20613012abff954128b02d1d1903bab09ad
Vishishta Shukla <vishishtashukla1@gmail.com>
2026-06-08 22:15:59 +0530

    Worked on day 7

---
+++ week_1/day_7/Recursive_reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int rev = 0;
+
+int reverse(int n) {
+    if (n == 0)
+        return rev;
+
+    rev = rev * 10 + n % 10;
+    reverse(n / 10);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Reverse = %d", reverse(num));
+
+    return 0;
+}
