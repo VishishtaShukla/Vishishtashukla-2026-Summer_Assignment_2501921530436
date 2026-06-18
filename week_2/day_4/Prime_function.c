cb8ab5929b1ee29ea9342d5a53c18bc4fe650bce
Vishishta Shukla <vishishtashukla1@gmail.com>
2026-06-18 21:54:45 +0530

    worked on week 2 day 4

---
+++ week_2/day_4/Prime_function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void prime(int n)
+{
+    int i, count = 0;
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+            count++;
+    }
+
+    if(count == 2)
+        printf("Prime Number");
+    else
+        printf("Not Prime Number");
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    prime(n);
+
+    return 0;
+}
